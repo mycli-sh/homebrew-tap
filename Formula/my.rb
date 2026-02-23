d8cca069c6835f00909ed009b0a9d8a187a89966
Fernando Nogueira <fernandonogueira@users.noreply.github.com>
2026-02-22 23:08:46 -0300

    chore: update formula URL and SHA256 checksum to v0.0.2

--- Formula/my.rb
+++ Formula/my.rb
@@ -1,8 +1,8 @@
 class My < Formula
   desc "CLI tool for defining, publishing, and running shell-based command specs"
   homepage "https://mycli.sh"
-  url "https://github.com/mycli-sh/mycli/archive/refs/tags/v0.0.0.tar.gz"
-  sha256 "PLACEHOLDER"
+  url "https://github.com/mycli-sh/mycli/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "04528989d036646fa2cd356b9bd08ec7a67fd8627e77a4ff591287d022a14b52"
   license "MIT"
 
   depends_on "go" => :build
